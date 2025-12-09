d10d6ce7f3dd8f802b4d7510cc0697d5e2809475
muhdsyahirk <muhdsyahirk@gmail.com>
2025-12-09 12:21:26 +0800

    All Responsive

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Scroll -> Background Nav
 const navBar = document.querySelector(".nav");
 const navMenu = document.querySelectorAll(".nav-menu-elem");
 
@@ -15,6 +16,7 @@ window.addEventListener("scroll", () => {
   }
 });
 
+// Responsive Sidebar
 function showSidebar() {
   const navSidebar = document.querySelector(".nav-sidebar");
   navSidebar.style.display = "flex";
@@ -23,3 +25,43 @@ function hideSidebar() {
   const navSidebar = document.querySelector(".nav-sidebar");
   navSidebar.style.display = "none";
 }
+
+// Scroll -> Reveal Sosmed
+const linkWidth = 630;
+
+if (window.innerWidth < linkWidth) {
+  const linkTexts = document.querySelectorAll(".barber-link-show-630px");
+
+  const observerLink = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        entry.target.classList.toggle(
+          "barber-link-transition",
+          entry.isIntersecting
+        );
+        // if (entry.isIntersecting) observerLink.unobserve(entry.target);
+      });
+    },
+    {
+      threshold: 1,
+    }
+  );
+
+  linkTexts.forEach((x) => {
+    observerLink.observe(x);
+  });
+}
+
+const operatingHours = document.querySelector(".home-in-content-3");
+
+const observerOperatingHours = new IntersectionObserver(
+  (entry) => {
+    entry.forEach((x) => {
+      x.target.classList.toggle("operating-hours-transition", x.isIntersecting);
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
+observerOperatingHours.observe(operatingHours);
